d42e9c3d1647f0ab0c53a74670e0b3ad59467e37
Kostiantyn Buriak <koslan843@gmail.com>
2023-05-01 04:46:13 +0300

    update

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
-// Get related Deals
-const relatedDeals = zoho.crm.getRelatedRecords({
-    Entity: "Deals",
-    Related_Name: "Deals",
-    Record_ID: "{RECORD_ID}"
-});
-
-// Calculate summary amounts and win rate
-let openDealsAmount = 0;
-let wonDealsAmount = 0;
-let lostDealsAmount = 0;
-let winRate = 0;
-let totalDealsAmount = 0;
-let totalDealsCount = relatedDeals.length;
-
-relatedDeals.forEach(function(deal) {
-    const stage = deal.Stage;
-    const amount = deal.Amount;
-    if (stage === "Open") {
-        openDealsAmount += amount;
-    } else if (stage === "Won") {
-        wonDealsAmount += amount;
-    } else if (stage === "Lost") {
-        lostDealsAmount += amount;
-    }
-});
-
-totalDealsAmount = openDealsAmount + wonDealsAmount + lostDealsAmount;
-
-if (totalDealsCount > 0) {
-    winRate = (wonDealsAmount / totalDealsAmount) * 100;
+string related_list.RelatedList_Function(map account)
+{
+accountId = account.get("Accounts.ID");
+dealsResp = zoho.crm.searchRecords("Deals","(Account_Name:equals:" + accountId + ")",1,100);
+openDealsAmount = 0;
+wonDealsAmount = 0;
+lostDealsAmount = 0;
+wonDealsCount = 0;
+lostDealsCount = 0;
+for each  deal in dealsResp
+{
+	dealAmount = deal.get("Amount");
+	dealStage = deal.get("Stage");
+	if(dealStage.contains("Open"))
+	{
+		openDealsAmount = openDealsAmount + dealAmount;
+	}
+	else if(dealStage.contains("Won"))
+	{
+		wonDealsAmount = wonDealsAmount + dealAmount;
+		wonDealsCount = wonDealsCount + 1;
+	}
+	else if(dealStage.contains("Lost"))
+	{
+		lostDealsAmount = lostDealsAmount + dealAmount;
+		lostDealsCount = lostDealsCount + 1;
+	}
 }
-
-// Update widget values
-document.getElementById("open-deals-amount").textContent = `$${openDealsAmount}`;
-document.getElementById("won-deals-amount").textContent = `$${wonDealsAmount}`;
-document.getElementById("lost-deals-amount").textContent = `$${lostDealsAmount}`;
-document.getElementById("win-rate").textContent = `${Math.round(winRate)}% (${wonDealsAmount} / ${totalDealsAmount})`;
-document.getElementById("win-rate").setAttribute("title", `${wonDealsAmount} Won Deals / ${totalDealsAmount} Total Deals`);
+totalDealsCount = wonDealsCount + lostDealsCount;
+winRate = wonDealsCount / totalDealsCount * 100;
+responseXML = "<record>";
+responseXML = responseXML + "<row no='0'><FL val='Open Deals Amount'>" + openDealsAmount + "</FL></row>";
+responseXML = responseXML + "<row no='1'><FL val='Won Deals Amount'>" + wonDealsAmount + "</FL></row>";
+responseXML = responseXML + "<row no='2'><FL val='Lost Deals Amount'>" + lostDealsAmount + "</FL></row>";
+responseXML = responseXML + "<row no='3'><FL val='Win Rate #'>" + wonDealsCount + "</FL></row>";
+responseXML = responseXML + "<row no='4'><FL val='Win Rate %'>" + winRate + "</FL></row>";
+responseXML = responseXML + "</record>";
+return responseXML;
+}
